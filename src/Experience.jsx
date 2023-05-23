73c4398b5dd159b67ccb1af72607488a1a05bad6
nizar7zak <nizar7zakout@gmail.com>
2023-05-23 09:40:48 +0200

    background color

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,6 +4,7 @@ export default function Experience()
 {
     return <>
 
+        <color args={ [ '#241a1a' ] } attach='background' />
         <OrbitControls makeDefault />
         
         <mesh>
