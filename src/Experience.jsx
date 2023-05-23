c10d7ddf5a460102388c9242eb6852a5007fa27a
nizar7zak <nizar7zakout@gmail.com>
2023-05-23 09:51:58 +0200

    environment city

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,15 @@
-import { useGLTF, OrbitControls } from '@react-three/drei'
+import { Center, Environment, useGLTF, OrbitControls } from '@react-three/drei'
 
 export default function Experience(){
     const laptop = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
-    console.log(laptop)
     return <>
-
+        <Environment preset='city' />
         <color args={ [ '#241a1a' ] } attach='background' />
         <OrbitControls makeDefault />
         
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-        <primitive object={laptop.scene} />
+        <Center>
+            <primitive object={laptop.scene} />
+        </Center>
 
     </>
 }
