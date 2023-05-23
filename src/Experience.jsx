7d73aeebea417710ac64ddafaa01e9bea9f3a4c7
nizar7zak <nizar7zakout@gmail.com>
2023-05-23 10:11:40 +0200

    presentation controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,19 +1,30 @@
-import { Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import { 
+    Float, 
+    Environment, 
+    useGLTF, 
+    PresentationControls
+} from '@react-three/drei'
 
 export default function Experience(){
     const laptop = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
     return <>
+
         <Environment preset='city' />
         <color args={ [ '#241a1a' ] } attach='background' />
-        <OrbitControls makeDefault />
-        
-        <Float rotationIntensity={ 0.4 }>
-            <primitive 
-                object={laptop.scene} 
-                position-y = { -1.2 }
-            />
-        </Float>
 
+        <PresentationControls
+            global
+            rotation={[ 0.13, 0.1, 0 ]}
+            polar={[ -0.4, 0.2 ]}
+            azimuth={[ -1, 0.75 ]}
+        >
+            <Float rotationIntensity={ 0.4 }>
+                <primitive 
+                    object={laptop.scene} 
+                    position-y = { -1.2 }
+                    />
+            </Float>
+        </PresentationControls>
 
     </>
 }
