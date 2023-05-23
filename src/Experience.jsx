86986d9e9541e1bde29615259cd7818f9296a79e
nizar7zak <nizar7zakout@gmail.com>
2023-05-23 09:54:23 +0200

    floating animation

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Center, Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import { Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
 
 export default function Experience(){
     const laptop = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
@@ -7,9 +7,13 @@ export default function Experience(){
         <color args={ [ '#241a1a' ] } attach='background' />
         <OrbitControls makeDefault />
         
-        <Center>
-            <primitive object={laptop.scene} />
-        </Center>
+        <Float rotationIntensity={ 0.4 }>
+            <primitive 
+                object={laptop.scene} 
+                position-y = { -1.2 }
+            />
+        </Float>
+
 
     </>
 }
