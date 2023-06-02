4832bf95da73eae9f89d26f43f7834fa9e1c7450
nizar7zak <nizar7zakout@gmail.com>
2023-06-02 14:36:57 +0200

    import model locally

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -10,9 +10,7 @@ import {
 } from '@react-three/drei'
 
 export default function Experience(){
-    const laptop = useGLTF(
-        'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
-    )
+    const laptop = useGLTF('./model.gltf')
     return <>
 
         <Environment 
